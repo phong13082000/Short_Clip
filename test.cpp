21189e757fa3816971adabed64954e207e8eb428
phong13082000 <84502367+phong13082000@users.noreply.github.com>
2022-04-20 00:21:01 +0700

    short_clip

--- test.cpp
+++ test.cpp
@@ -7,13 +7,11 @@ using namespace std;
 double user_trace[30] ={3.72687272, 10.97615345,  3.73311376,  7.93133749,  5.46250672, 5.03175729,  4.2033281 ,  0.23471722,  3.13193818,  4.74024695, 3.63160728,  4.61824191,  3.49232399,  6.1516166 ,  5.94345566, 10.91569073,  1.71582105,  5.78847004,  6.85737497,  5.37536954, 4.60972807,  8.24144345,  6.87478599, 10.73155018,  9.92433724, 4.80577454,  8.29860233,  6.84510747,  5.85787661,  3.03570681};
 double BW[250]; // network trace
 double Buf[30]; // thoi gian buffer cua tung video
-int a;
+int a; // next video dang duoc buffer
 int Bseg ; // gioi han so segment dc buffer
 int K; // so luong next video dc buffer
-double seg=0; // segment = 1 se buffer 1s cho video
 double bitrate = 1000; // 1000 kb/s
-double time_step=0.1;
-int video =0;
+double time_step=0.001;
 int Tong_video =30;
 double s=0; // tong thoi gian user xem
 void Read_BW(double BW[250])// doc file network trace
@@ -50,7 +48,7 @@ int main()
             double tx=0; // thoi gian xem video hien tai
             double seg=0; // segment = 1 se buffer 1s cho video
             int video =0;
-            int v=0;
+            int v=0; // vi tri video dc buffer sau khi chuyen video
 
             for (int i=0; i<30;i++)
             {
